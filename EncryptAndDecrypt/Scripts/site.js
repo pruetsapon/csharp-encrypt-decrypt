dcd0b1e5284a570eb10a2fa6ad36feb137a723e3
pruetsapon <pruetsapon@hotmail.com>
2020-02-12 20:50:50 +0700

    encrypt and decrypt with js, csharp.

---
+++ EncryptAndDecrypt/Scripts/site.js
@@ -0,0 +1,56 @@
+﻿var key = CryptoJS.enc.Utf8.parse('7061737323313233');
+var iv = CryptoJS.enc.Utf8.parse('7061737323313233');
+
+const encryptWithJavascript = () => {
+    var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse($('#encrypt').val()), key, {
+        keySize: 128 / 8,
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    $('#decrypt').val(encrypted);
+
+    //console.log('Encrypted :' + encrypted);
+    //console.log('Key :' + encrypted.key);
+    //console.log('Salt :' + encrypted.salt);
+    //console.log('iv :' + encrypted.iv);
+}
+
+const decryptWithJavascript = () => {
+    var decrypted = CryptoJS.AES.decrypt($('#decrypt').val(), key, {
+        keySize: 128 / 8,
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    $('#encrypt').val(decrypted.toString(CryptoJS.enc.Utf8));
+
+    //console.log('Decrypted : ' + decrypted);
+    //console.log('utf8 = ' + decrypted.toString(CryptoJS.enc.Utf8));
+}
+
+const encryptWithCsharp = () => {
+    $.ajax({
+        type: "GET",
+        url: `/Home/Encrypt?encryptText=${$('#encrypt').val()}`,
+        success: (data) => {
+            $('#decrypt').val(data);
+        },
+        error: (errMsg) => {
+            console.log(errMsg);
+        }
+    });
+}
+
+const decryptWithCsharp = () => {
+    $.ajax({
+        type: "GET",
+        url: `/Home/Decrypt?decryptText=${$('#decrypt').val()}`,
+        success: (data) => {
+            $('#encrypt').val(data);
+        },
+        error: (errMsg) => {
+            console.log(errMsg);
+        }
+    });
+}
---
+++ EncryptAndDecrypt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EncryptAndDecrypt.Startup))]
+namespace EncryptAndDecrypt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
